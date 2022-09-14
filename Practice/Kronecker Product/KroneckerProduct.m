863d3157a8486baf2df6e6a2acc2abd0213d2abf
Ashley P. Mathews <73625048+ashpmath@users.noreply.github.com>
2022-09-14 13:57:04 -0500

    Add files via upload

---
+++ Practice/Kronecker Product/KroneckerProduct.m
@@ -0,0 +1,12 @@
+% TENSOR PRODUCT PROGRAM
+% Programmer: Ashley Mathews
+% Date: 10/25/2021
+
+% Define Matrix Dimensions
+A =[1 2; -1 0]
+B =[4 -3; 2 3]
+
+% Compute Product of Matrices
+K = kron(A,B)
+
+
